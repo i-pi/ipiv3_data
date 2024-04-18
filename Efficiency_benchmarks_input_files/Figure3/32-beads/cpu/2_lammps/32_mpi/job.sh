9249812fe0625190a07f9938c35bc51b5cfd86cb
Davide Tisi <47503434+DavideTisi@users.noreply.github.com>
2024-04-18 11:53:36 +0200

    added cpu ipi-deepmd files

---
+++ Efficiency_benchmarks_input_files/Figure3/32-beads/cpu/2_lammps/32_mpi/job.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=da_Li
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=72
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=0
+#SBATCH --time=8:30:00
+#SBATCH --exclusive
+#SBATCH --qos=serial
+
+module load gcc/11.3.0  openmpi/4.1.3
+source /home/tisi/ipi-paper-test/venv-cpu/bin/activate  
+rm /tmp/ipi_driver
+export OMP_NUM_THREADS=1
+echo 'OMP_NUM_THREADS='$OMP_NUM_THREADS
+export TF_INTER_OP_PARALLELISM_THREADS=1
+export TF_INTRA_OP_PARALLELISM_THREADS=1
+
+IPI=/home/tisi/ipi-paper-test/i-pi-main-3.0/bin/i-pi
+#IPI=/home/tisi/ipi-paper-test/venv-cpu/bin/i-pi
+srun -n 1 --exclusive python -u $IPI ../input_xml.xml &> log.ipi &
+sleep 15
+
+for i in $(seq 2);
+do
+echo 'run deepmd number '$i
+#srun -n ${SLURM_CPUS_PER_TASK} dp_ipi ../water.json >dp$i.out &
+srun -n 32 --exclusive lmp -in ../input-ipi-lammps.lammps >dp$i.out &
+sleep 2 
+done
+
+wait
--- Efficiency_benchmarks_input_files/Figure3/README.md
+++ Efficiency_benchmarks_input_files/Figure3/README.md
@@ -1,3 +1,5 @@
 # Benchmarks 
 Inputs, scripts, and model information necessary to reproduce benchmark runs present in the main text.
 
+# DeepMD
+The model used in the simulations is taken from https://github.com/deepmodeling-activity/deepmd-kit-v2-paper/blob/main/models/03/frozen_model.pb 
