f5cda63b72f8f0e343fdbf5c77ede1836a9877c6
Yair Litman <yairlitman@gmail.com>
2024-04-23 12:11:58 +0100

    update elphmod figure

--- Figures/Fig17_Elphmod_cdw/modify_paper_plot_Sq.py
+++ Figures/Fig17_Elphmod_cdw/modify_paper_plot_Sq.py
@@ -9,6 +9,25 @@ plt.style.use('tableau-colorblind10')
 from matplotlib import cm
 from matplotlib import colors
 
+# Define format
+plt.rc("font", **{"family": "sans-serif", "sans-serif": ["Arial"]})
+tick_major_size = 10
+tick_minor_size = 7
+labelsize = 20
+fontsize = 18
+legend_fontsize = 15
+
+plt.rcParams["font.size"] = fontsize
+plt.rc("axes", linewidth=2, labelpad=10)
+#plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
+plt.rc("xtick.major", size=tick_major_size, width=2)
+plt.rc("xtick.minor", size=tick_minor_size, width=2)
+plt.rc("ytick.major", size=tick_major_size, width=2)
+plt.rc("ytick.minor", size=tick_minor_size, width=2)
+
+
+
 """""""""""""""""""""""""""""""""""""""
 Parameters
 """""""""""""""""""""""""""""""""""""""
@@ -64,249 +83,8 @@ BZ_color = 'k'
 panel_label_list = ['a', 'b', 'c']
 
 
-#gs_kw = dict(width_ratios=[1, 1, 1, 1.2], height_ratios=[1,1,1])
 gs_kw = dict(width_ratios=[1, 1, 1.2], height_ratios=[1])
-#fig, (ax) = plt.subplots(3,4, figsize=(figx,figy), gridspec_kw=gs_kw)
 fig, (ax) = plt.subplots(1,3, figsize=(figx,figy), gridspec_kw=gs_kw)
-
-"""""""""""""""""""""""""""""""""""""""
-FIRST ROW!
-"""""""""""""""""""""""""""""""""""""""
-                    
-# for ii,(SRT,panel) in enumerate(zip(SRT_list,panel_label_list)):
-#
-#     print(SRT)
-#
-#     """""""""""""""""""""""""""""""""""""""
-#     Load structure factor
-#     """""""""""""""""""""""""""""""""""""""
-#
-#     S_tmp = np.load('S_TaS2_18x18_N_steps_start=70_N_steps_inter=10_N_steps_end=1056_CLASSICAL/S_%d.npy' % SRT)
-#     S['class'] = (S_tmp.sum(axis=0)).reshape(N1*N1) / S_tmp.shape[0]
-#
-#     S_tmp = np.load('S_TaS2_18x18_N_steps_start=70_N_steps_inter=10_N_steps_end=496_PIMD_beads/S_%d.npy' % SRT)
-#     S['quant'] = (S_tmp.sum(axis=0)).reshape(N1*N1) / S_tmp.shape[0]
-#
-#     S_tmp = np.load('S_TaS2_18x18_N_steps_start=70_N_steps_inter=10_N_steps_end=1056_CLASSICAL/S_%d_BG.npy' % SRT)
-#     S['class_BG'] = (S_tmp.sum(axis=0)).reshape(N1*N1) / S_tmp.shape[0]
-#
-#     S_tmp = np.load('S_TaS2_18x18_N_steps_start=70_N_steps_inter=10_N_steps_end=496_PIMD_beads/S_%d_BG.npy' % SRT)
-#     S['quant_BG'] = (S_tmp.sum(axis=0)).reshape(N1*N1) / S_tmp.shape[0]
-#
-# #    S['class'] = S['class_BG']
-# #    S['quant'] = S['quant_BG']
-#
-#     """""""""""""""""""""""""""""""""""""""
-#     Draw Brillouin zone
-#     """""""""""""""""""""""""""""""""""""""
-#
-#     outline = elphmod.bravais.BZ()
-#     outline = np.array(outline) / (a / nk)
-#     outline = list(outline)
-#
-#     ax[0,ii].fill_between(*zip(*outline), color = BZ_color)
-#     ax[0,ii].plot(*zip(*outline), color='gray',
-#     linewidth = 1, alpha=0.4)
-#
-#     """""""""""""""""""""""""""""""""""""""
-#     Choose quantity to plot
-#     """""""""""""""""""""""""""""""""""""""
-#
-#     S_plot = S['class']**(class_exponent) / S['quant']**(quant_exponent)
-#
-#
-#     """""""""""""""""""""""""""""""""""""""
-#     Add BZ points from the edge
-#     """""""""""""""""""""""""""""""""""""""
-#
-#     q = np.load('output_kpoints/q_voronoi_%s%s2_%dx%d.npy' % ('Ta','S',N1,N1))
-#     q = np.reshape(q,(N1*N1,3))
-#     qx = q[:,0] / (2*np.pi)
-#     qy = q[:,1] / (2*np.pi)
-#
-#
-#     # K points
-#     qx = np.append(qx, np.array([-1.96841415e-01]))
-#     qy = np.append(qy, np.array([0]))
-#     S_plot = np.append(S_plot, np.array([S_plot[228]]))
-#
-#     qx = np.append(qx, np.array([1.96841415e-01]))
-#     qy = np.append(qy, np.array([0]))
-#     S_plot = np.append(S_plot, np.array([S_plot[228]]))
-#
-#     upper_edge = [228,263,298,9,44,79,114]
-#     for shift_index in upper_edge:
-#         qx = np.append(qx, np.array([qx[shift_index]]))
-#         qy = np.append(qy, np.array([-qy[shift_index]]))
-#         S_plot = np.append(S_plot, np.array([S_plot[shift_index]]))
-#
-#     left_edge = [133,152,171,190,209,79,114,228]
-#     for shift_index in left_edge:
-#         qx = np.append(qx, np.array([qx[shift_index]]))
-#         qy = np.append(qy, np.array([-qy[shift_index]]))
-#         S_plot = np.append(S_plot, np.array([S_plot[shift_index]]))
-#
-#     right_edge = [114,130,146,162,196,212]
-#     for shift_index in right_edge:
-#         qx = np.append(qx, np.array([qx[shift_index]]))
-#         qy = np.append(qy, np.array([-qy[shift_index]]))
-#         S_plot = np.append(S_plot, np.array([S_plot[shift_index]]))
-#
-#     """""""""""""""""""""""""""""""""""""""
-#     Plot scatter
-#     """""""""""""""""""""""""""""""""""""""
-#
-#     if divide:
-#         im = ax[0,ii].scatter(qx, qy, c=abs(S_plot), s=ms, cmap='seismic', norm=colors.TwoSlopeNorm(vcenter = 1, vmin = 0.0, vmax=3.53))
-#     else:
-#         im = ax[0,ii].scatter(qx, qy, c=abs(S_plot), s=ms, cmap='gist_heat',  norm=colors.LogNorm(vmin=5e-7, vmax=1))
-#     # colorbar
-#     if SRT == 25:
-#         if divide:
-#             ticks=[0.0, 0.25,0.5, 0.75, 1.0, 1.5, 2.0,2.5,3.0,3.5]
-#             cb = fig.colorbar(im, ax=ax, aspect=15, ticks=ticks)
-#             cb.ax.tick_params(labelsize=cb_ticks_font_size)
-#             cb.ax.set_yticklabels(ticks)  # vertically oriented colorbar
-#         else:
-#             cb = fig.colorbar(im, ax=ax[0,ii], aspect=10)
-#             cb.ax.tick_params(labelsize=cb_ticks_font_size)
-#
-#
-#
-#     ax[0,ii].axis('off')
-#     ax[0,ii].text(0.01, 0.9, '(%s)' % (panel), transform=ax[0,ii].transAxes,
-#                 size=fs_panel, color = 'black')
-#     ax[0,ii].text(0.8, 0.9, '%3.fK' % T_list[SRT],size=fs_panel-2,
-#     transform=ax[0,ii].transAxes, color = 'black')
-#
-#
-#     plt.tick_params(top=False, bottom=False, left=False, right=False,
-#                     labelleft=False, labelbottom=False)
-                    
-"""""""""""""""""""""""""""""""""""""""
-SECOND ROW!
-"""""""""""""""""""""""""""""""""""""""
-
-# divide = False
-# class_exponent = 0
-# quant_exponent = -1
-# save = 'quant'
-# BZ_color = 'k'
-# panel_label_list = ['d', 'e', 'f']
-#
-# for ii,(SRT,panel) in enumerate(zip(SRT_list,panel_label_list)):
-#
-#     print(SRT)
-#
-#     """""""""""""""""""""""""""""""""""""""
-#     Load structure factor
-#     """""""""""""""""""""""""""""""""""""""
-#
-#     S_tmp = np.load('S_TaS2_18x18_N_steps_start=70_N_steps_inter=10_N_steps_end=1056_CLASSICAL/S_%d.npy' % SRT)
-#     S['class'] = (S_tmp.sum(axis=0)).reshape(N1*N1) / S_tmp.shape[0]
-#
-#     S_tmp = np.load('S_TaS2_18x18_N_steps_start=70_N_steps_inter=10_N_steps_end=496_PIMD_beads/S_%d.npy' % SRT)
-#     S['quant'] = (S_tmp.sum(axis=0)).reshape(N1*N1) / S_tmp.shape[0]
-#
-#     S_tmp = np.load('S_TaS2_18x18_N_steps_start=70_N_steps_inter=10_N_steps_end=1056_CLASSICAL/S_%d_BG.npy' % SRT)
-#     S['class_BG'] = (S_tmp.sum(axis=0)).reshape(N1*N1) / S_tmp.shape[0]
-#
-#     S_tmp = np.load('S_TaS2_18x18_N_steps_start=70_N_steps_inter=10_N_steps_end=496_PIMD_beads/S_%d_BG.npy' % SRT)
-#     S['quant_BG'] = (S_tmp.sum(axis=0)).reshape(N1*N1) / S_tmp.shape[0]
-#
-# #    S['class'] = S['class_BG']
-# #    S['quant'] = S['quant_BG']
-#
-#     """""""""""""""""""""""""""""""""""""""
-#     Draw Brillouin zone
-#     """""""""""""""""""""""""""""""""""""""
-#
-#     outline = elphmod.bravais.BZ()
-#     outline = np.array(outline) / (a / nk)
-#     outline = list(outline)
-#
-#     ax[1,ii].fill_between(*zip(*outline), color = BZ_color)
-#     ax[1,ii].plot(*zip(*outline), color='gray',
-#     linewidth = 1, alpha=0.4)
-#
-#     """""""""""""""""""""""""""""""""""""""
-#     Choose quantity to plot
-#     """""""""""""""""""""""""""""""""""""""
-#
-#     S_plot = S['class']**(class_exponent) / S['quant']**(quant_exponent)
-#
-#
-#     """""""""""""""""""""""""""""""""""""""
-#     Add BZ points from the edge
-#     """""""""""""""""""""""""""""""""""""""
-#
-#     q = np.load('output_kpoints/q_voronoi_%s%s2_%dx%d.npy' % ('Ta','S',N1,N1))
-#     q = np.reshape(q,(N1*N1,3))
-#     qx = q[:,0] / (2*np.pi)
-#     qy = q[:,1] / (2*np.pi)
-#
-#
-#     # K points
-#     qx = np.append(qx, np.array([-1.96841415e-01]))
-#     qy = np.append(qy, np.array([0]))
-#     S_plot = np.append(S_plot, np.array([S_plot[228]]))
-#
-#     qx = np.append(qx, np.array([1.96841415e-01]))
-#     qy = np.append(qy, np.array([0]))
-#     S_plot = np.append(S_plot, np.array([S_plot[228]]))
-#
-#     upper_edge = [228,263,298,9,44,79,114]
-#     for shift_index in upper_edge:
-#         qx = np.append(qx, np.array([qx[shift_index]]))
-#         qy = np.append(qy, np.array([-qy[shift_index]]))
-#         S_plot = np.append(S_plot, np.array([S_plot[shift_index]]))
-#
-#     left_edge = [133,152,171,190,209,79,114,228]
-#     for shift_index in left_edge:
-#         qx = np.append(qx, np.array([qx[shift_index]]))
-#         qy = np.append(qy, np.array([-qy[shift_index]]))
-#         S_plot = np.append(S_plot, np.array([S_plot[shift_index]]))
-#
-#     right_edge = [114,130,146,162,196,212]
-#     for shift_index in right_edge:
-#         qx = np.append(qx, np.array([qx[shift_index]]))
-#         qy = np.append(qy, np.array([-qy[shift_index]]))
-#         S_plot = np.append(S_plot, np.array([S_plot[shift_index]]))
-#
-#     """""""""""""""""""""""""""""""""""""""
-#     Plot scatter
-#     """""""""""""""""""""""""""""""""""""""
-#
-#     if divide:
-#         im = ax[1,ii].scatter(qx, qy, c=abs(S_plot), s=ms, cmap='seismic', norm=colors.TwoSlopeNorm(vcenter = 1, vmin = 0.0, vmax=3.53))
-#     else:
-#         im = ax[1,ii].scatter(qx, qy, c=abs(S_plot), s=ms, cmap='gist_heat',  norm=colors.LogNorm(vmin=5e-7, vmax=1))
-#     # colorbar
-#     if SRT == 25:
-#         if divide:
-#             ticks=[0.0, 0.25,0.5, 0.75, 1.0, 1.5, 2.0,2.5,3.0,3.5]
-#             cb = fig.colorbar(im, ax=ax, aspect=15, ticks=ticks)
-#             cb.ax.tick_params(labelsize=cb_ticks_font_size)
-#             cb.ax.set_yticklabels(ticks)  # vertically oriented colorbar
-#         else:
-#             cb = fig.colorbar(im, ax=ax[1,ii], aspect=10)
-#             cb.ax.tick_params(labelsize=cb_ticks_font_size)
-#
-#
-#
-#     ax[1,ii].axis('off')
-#     ax[1,ii].text(0.01, 0.9, '(%s)' % (panel), transform=ax[1,ii].transAxes,
-#                 size=fs_panel, color = 'black')
-#     ax[1,ii].text(0.8, 0.9, '%3.fK' % T_list[SRT],size=fs_panel-2,
-#     transform=ax[1,ii].transAxes, color = 'black')
-#
-#
-#     plt.tick_params(top=False, bottom=False, left=False, right=False,
-#                     labelleft=False, labelbottom=False)
-                    
-"""""""""""""""""""""""""""""""""""""""
-THIRD ROW!
-"""""""""""""""""""""""""""""""""""""""
 
 divide = True
 class_exponent = 1
@@ -335,8 +113,6 @@ for ii,(SRT,panel) in enumerate(zip(SRT_list,panel_label_list)):
     S_tmp = np.load('S_TaS2_18x18_N_steps_start=70_N_steps_inter=10_N_steps_end=496_PIMD_beads/S_%d_BG.npy' % SRT) 
     S['quant_BG'] = (S_tmp.sum(axis=0)).reshape(N1*N1) / S_tmp.shape[0]
 
-#    S['class'] = S['class_BG']
-#    S['quant'] = S['quant_BG']
     
     """""""""""""""""""""""""""""""""""""""
     Draw Brillouin zone
@@ -426,7 +202,3 @@ for ii,(SRT,panel) in enumerate(zip(SRT_list,panel_label_list)):
 
 plt.tight_layout()
 plt.savefig('fig_cdw_abc.pdf')
-#plt.savefig('fig9_paper.png')
-#    plt.show()
-#    plt.close()
-
--- Figures/Fig17_Elphmod_cdw/paper_intensity_manuscript.py
+++ Figures/Fig17_Elphmod_cdw/paper_intensity_manuscript.py
@@ -12,21 +12,34 @@ plt.style.use('tableau-colorblind10')
 """""""""""""""""""""""""""""""""""""""
 Parameters
 """""""""""""""""""""""""""""""""""""""
+# Define format
+plt.rc("font", **{"family": "sans-serif", "sans-serif": ["Arial"]})
+tick_major_size = 10
+tick_minor_size = 7
+labelsize = 20
+fontsize = 18
+legend_fontsize = 15
 
+plt.rcParams["font.size"] = fontsize
+plt.rc("axes", linewidth=2, labelpad=10)
+#plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
+plt.rc("xtick.major", size=tick_major_size, width=2)
+plt.rc("xtick.minor", size=tick_minor_size, width=2)
+plt.rc("ytick.major", size=tick_major_size, width=2)
+plt.rc("ytick.minor", size=tick_minor_size, width=2)
 
-figx = 7.3
-figy = 2.4
 capsize = 2
 ms = 2
-fs = 8.0 
-fs_tick = 7.5 # fontsize tick
+fs = fontsize 
+fs_tick = tick_major_size # fontsize tick
 fs_title = 10 # title
-fs_abcd = 10 # fontsize abdc label
+fs_abcd = fontsize # fontsize abdc label
 
 #figx = 3.0
 #figy = 2.4
 figx = 6.0
-figy=2.5
+figy = 5.0
 
 linewidth = 1.0 
 """""""""""""""""""""""""""""""""""""""
@@ -60,35 +73,37 @@ Plot 1 PANEL
 """""""""""""""""""""""""""""""""""""""
 
 gs_kw = dict(width_ratios=[1], height_ratios=[1])
-fig, (ax) = plt.subplots(1,1, figsize=(figx,figy), gridspec_kw=gs_kw)
+fig, ax = plt.subplots(1, figsize=(figx,figy), gridspec_kw=gs_kw)
 
 int_type = 'Total'
 
 for y, ylabel in enumerate([
-r'$<S(q=2/3\Gamma M)>$',
+r'$<S(q=2/3\Gamma M)>\times 10^4$',
 ]):
     ax.set_ylabel(ylabel, fontsize=fs)
-    
+factor=10**4    
 for x, xlabel in enumerate([
-r'Temperature in K', 
-r'Temperature in K',
-r'Temperature in K',
+r'Temperature (K)', 
+r'Temperature (K)',
+r'Temperature (K)',
 ]):
-    ax.set_xlabel(xlabel, fontsize=fs)
+    ax.set_xlabel(xlabel, fontsize=fontsize)
 
 for x in range(1):
     # Set the y-axis limits
-    ax.set_ylim(0, 3.5e-4)  # Adjust the values as needed
+    ax.set_ylim(0, 3.5)  # Adjust the values as needed
     ax.set_xlim(50, 200)  # Adjust the values as needed
     # set tick size
     ax.tick_params(axis='both', which='major', labelsize=fs_tick)    
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
 
 
 #ax.axvspan(80, 85, facecolor='blue', alpha=0.3)
 #ax.axvspan(93, 98, facecolor='cornflowerblue', alpha=0.3)
 
 
-ax.errorbar(T_list['intensity_%s' % supercell], Intensity['CLASSICAL_%s_%s' % (int_type, supercell)] , Std['CLASSICAL_block_%s_%s' % (int_type, supercell)],
+ax.errorbar(T_list['intensity_%s' % supercell],factor* Intensity['CLASSICAL_%s_%s' % (int_type, supercell)] , factor*Std['CLASSICAL_block_%s_%s' % (int_type, supercell)],
 marker = 'o',
 color = 'cornflowerblue', 
 #    linestyle = 'None',
@@ -97,33 +112,24 @@ linewidth = linewidth,
 markersize = ms,     
 capsize = capsize)
 
-ax.errorbar(T_list['intensity_%s' % supercell], Intensity['PIMD_beads_%s_%s' % (int_type, supercell)] , Std['PIMD_beads_block_%s_%s' % (int_type, supercell)],
+ax.errorbar(T_list['intensity_%s' % supercell], factor*Intensity['PIMD_beads_%s_%s' % (int_type, supercell)] , factor*Std['PIMD_beads_block_%s_%s' % (int_type, supercell)],
 marker = 'o',
 color = 'blue', 
-#    linestyle = 'None',
 linestyle = '--',
 linewidth = linewidth,
 markersize = ms,     
 capsize = capsize,
 )
 
-# set title
-#ax.set_title('%s' % int_type, fontsize=fs_title)
 # set panel
-ax.text(-0.13, 1.1, '(%s)' % panels[0], transform=ax.transAxes, 
-                size=fs_abcd)
+ax.text(-0.13, 1.1, '(%s)' % panels[0], transform=ax.transAxes, size=fs_abcd)
    
 # y-axis as power             
 ax.yaxis.major.formatter.set_powerlimits((0,0))
-ax.yaxis.offsetText.set_fontsize(fontsize=fs_tick)
-
-
-
+ax.yaxis.offsetText.set_fontsize(fontsize=fontsize)
 
 # experimental value?
 ax.axvline(x=75, color = 'black', linewidth = linewidth, linestyle = 'dashed')
-#    ax[mm].axvline(x=85, color = 'blue',  linewidth = linewidth, linestyle = 'dashed')  
-# ax.axvline(x=101.5, color = 'cornflowerblue', linewidth = linewidth, linestyle = 'dashed')  
 
 
 plt.tight_layout()
