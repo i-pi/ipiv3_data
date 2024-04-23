5195d109940316be1f3f5360bc914d50b5f5294b
Yair Litman <yairlitman@gmail.com>
2024-04-23 10:29:53 +0100

    update plots

--- Figures/Fig02_simple_overhead_scheme/make_fig.py
+++ Figures/Fig02_simple_overhead_scheme/make_fig.py
@@ -1,12 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+# Define format
+plt.rc("font", **{"family": "sans-serif", "sans-serif": ["Arial"]})
+tick_major_size = 10
+tick_minor_size = 7
+labelsize = 20
+fontsize = 18
+legend_fontsize = 15
 
-fig, ax = plt.subplots(1,1, figsize=(4,3), constrained_layout=True)
+plt.rcParams["font.size"] = fontsize
+plt.rc("axes", linewidth=2, labelpad=10)
+#plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
+plt.rc("xtick.major", size=tick_major_size, width=2)
+plt.rc("xtick.minor", size=tick_minor_size, width=2)
+plt.rc("ytick.major", size=tick_major_size, width=2)
+plt.rc("ytick.minor", size=tick_minor_size, width=2)
+
+fig,ax = plt.subplots(figsize=(6,5))
+
+fig, ax = plt.subplots(1, figsize=(7,5), constrained_layout=True)
 x = np.geomspace(1e-5, 1e2, 100)
 
-ax.loglog(x, 86.4e3*1e-6/x, ':', c='gray', label=r"ideal, $\tau_\mathrm{O}=0$")
-for e, c, t in zip([-5,-3,-1, 1], ["#0000B0", "#401090", "#802040", "#FF4010"], ["0.01$ ms","1$ ms", "100$ ms", "10$ s"]):
+ax.loglog(x, 86.4e3*1e-6/x, ':', c='gray', label=r"$\tau_\mathrm{O}=0$ (ideal)")
+for e, c, t in zip([-5,-3,-1, 1], ["#0000B0", "#401090", "#802040", "#FF4010"], ["10^{-2}$ ms","10^0$ ms", "10^2$ ms", "10^4$ ms"]):
     ax.loglog(x, 86.4e3*1e-6/(x+10.0**e), '--', c=c, label=r"$\tau_\mathrm{O}="+t)
 
 ax.fill_between(x, 86.4e3*1e-6/(x+2e-3), 86.4e3*1e-6/(x+2e-2), color="gray", alpha=0.2, lw=0)
@@ -16,6 +34,6 @@ ax.text(10, 5e-4, "ab initio", ha="center")
 ax.text(0.03, 5e-4, "MLIP", ha="center")
 ax.text(5e-5, 5e-4, "forcefields", ha="center")
 ax.legend(labelspacing=0.1)
-ax.set_xlabel(r"force evaluation time / s")
-ax.set_ylabel(r"simulation throughput / (ns/day)")
+ax.set_xlabel(r"Force evaluation time (s)")
+ax.set_ylabel(r"Simulation throughput  (ns/day)")
 fig.savefig("simple-overhead-scheme.pdf")
