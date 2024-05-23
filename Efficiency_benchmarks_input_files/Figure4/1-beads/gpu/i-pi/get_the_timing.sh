eae0063e7d3f8ae05761731d70e4a05eed01db01
Davide Tisi <47503434+DavideTisi@users.noreply.github.com>
2024-05-23 11:50:59 +0200

    new deepmd gpu data after lammps fix

--- Efficiency_benchmarks_input_files/Figure4/1-beads/gpu/i-pi/get_the_timing.sh
+++ Efficiency_benchmarks_input_files/Figure4/1-beads/gpu/i-pi/get_the_timing.sh
@@ -1,1 +1,1 @@
-echo '# t/step [s]'  > timing_ipi.out ;  for i in 1 2 4 8 16 ; do cd $i'_mpi' ;grep 't/step' log.ipi | tail -n +4 | head -n -1 | awk 'BEGINIG{c=0;m=0}{c+=1 ; m+=$9}END{print '$i', m/c}'>> ../timing_ipi.out ; cd .. ; done 
+echo '# t/step [s]'  > timing_ipi.out ;  for i in 1 2 4 8 ; do cd $i'_mpi' ;grep 't/step' log.ipi | tail -n +4 | head -n -1 | awk 'BEGINIG{c=0;m=0}{c+=1 ; m+=$9}END{print '$i', m/c}'>> ../timing_ipi.out ; cd .. ; done 
